f2116927e27d1e5413240933a024eb8869ea8abd
Jozef Steinhübl <xhyrom.hyro@gmail.com>
2023-09-02 11:58:07 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,6 @@ plugins {
     `java-library`
 
     `maven-publish`
-    signing
-    alias(libs.plugins.nexuspublish)
 }
 
 group = "dev.hollowcube"
@@ -41,74 +39,4 @@ java {
 tasks.test {
     maxHeapSize = "2g"
     useJUnitPlatform()
-}
-
-nexusPublishing {
-    this.packageGroup.set("dev.hollowcube")
-
-    repositories.sonatype {
-        nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-        snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-
-        if (System.getenv("SONATYPE_USERNAME") != null) {
-            username.set(System.getenv("SONATYPE_USERNAME"))
-            password.set(System.getenv("SONATYPE_PASSWORD"))
-        }
-    }
-}
-
-publishing.publications.create<MavenPublication>("maven") {
-    groupId = "dev.hollowcube"
-    artifactId = "polar"
-    version = project.version.toString()
-
-    from(project.components["java"])
-
-    pom {
-        name.set(artifactId)
-        description.set(project.description)
-        url.set("https://github.com/hollow-cube/polar")
-
-        licenses {
-            license {
-                name.set("MIT")
-                url.set("https://github.com/hollow-cube/polar/blob/main/LICENSE")
-            }
-        }
-
-        developers {
-            developer {
-                id.set("mworzala")
-                name.set("Matt Worzala")
-                email.set("matt@hollowcube.dev")
-            }
-        }
-
-        issueManagement {
-            system.set("GitHub")
-            url.set("https://github.com/hollow-cube/polar/issues")
-        }
-
-        scm {
-            connection.set("scm:git:git://github.com/hollow-cube/polar.git")
-            developerConnection.set("scm:git:git@github.com:hollow-cube/polar.git")
-            url.set("https://github.com/hollow-cube/polar")
-            tag.set(System.getenv("TAG_VERSION") ?: "HEAD")
-        }
-
-        ciManagement {
-            system.set("Github Actions")
-            url.set("https://github.com/hollow-cube/polar/actions")
-        }
-    }
-}
-
-signing {
-    isRequired = System.getenv("CI") != null
-
-    val privateKey = System.getenv("GPG_PRIVATE_KEY")
-    val keyPassphrase = System.getenv()["GPG_PASSPHRASE"]
-    useInMemoryPgpKeys(privateKey, keyPassphrase)
-
-    sign(publishing.publications)
 }
